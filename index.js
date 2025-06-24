7aa1ca9725439d6f60cf94e6e96a6be2b57503c0
Punit Katiyar <punit.kat@gmail.com>
2025-06-24 18:04:57 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const mongoose = require('mongoose')
 const Student = require("./models/student.model")
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static('public'))
 
-const mongoose = require('mongoose')
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/unit')
     .then(() => console.log("database connected"))
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
@@ -36,6 +38,24 @@ app.get('/view/:id', async (req, res) => {
     // res.json(students);
     res.render('profile', { student });
 })
+
+app.get('/edit/:id', async (req, res) => {
+    const student = await Student.findOne({ _id: req.params.id })
+    res.render('editContact', { student });
+})
+
+
+app.post('/edit/:id', async (req, res) => {
+    await Student.findByIdAndUpdate(req.params.id, req.body)
+    res.redirect("/view")
+})
+
+app.get('/delete/:id', async (req, res) => {
+    await Student.findByIdAndDelete(req.params.id)
+    res.redirect("/view")
+})
+
+
 
 
 
